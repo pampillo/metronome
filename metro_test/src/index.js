b18eef8fa0aea65f400fb6695418c0a068ca5ba5
Raul Barrantes <raul.pampillo@gmail.com>
2018-10-30 21:02:17 -0600

    Setup of metro_test files
    Creation of metro_test.js and metro_test.css

--- metro_test/src/index.js
+++ metro_test/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import metro_test from './metro_test';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<metro_test />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+
+
+
---
+++ metro_test/src/metro_test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './metro_test.css';
+
+class metro_test extends Component {
+  render() {
+    return <div className="metro_test">hi</div>;
+  }
+}
+
+export default metro_test;
